24021997e86f66ab44d70e83d57d5656e6d38b18
ChiYann <45175688+ChiyannQ@users.noreply.github.com>
2019-03-27 17:51:02 +0800

    Update __init__.py

--- pytorch-CycleGAN-and-pix2pix/data/__init__.py
+++ pytorch-CycleGAN-and-pix2pix/data/__init__.py
@@ -69,14 +69,16 @@ class CustomDatasetDataLoader():
         Step 2: create a multi-threaded data loader.
         """
         self.opt = opt
+       
         dataset_class = find_dataset_using_name(opt.dataset_mode)
         self.dataset = dataset_class(opt)
+        
         print("dataset [%s] was created" % type(self.dataset).__name__)
         self.dataloader = torch.utils.data.DataLoader(
-            self.dataset,
-            batch_size=opt.batch_size,
-            shuffle=not opt.serial_batches,
-            num_workers=int(opt.num_threads))
+              self.dataset,
+              batch_size=opt.batch_size,
+              shuffle=not opt.serial_batches,
+              num_workers=int(opt.num_threads))
 
     def load_data(self):
         return self
